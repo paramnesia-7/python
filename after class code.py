a158a408fd5174781de1f3cdb85b56475e841089
paramnesia-7 <rajmoiuksahagmail.com>
2025-11-21 20:11:43 +0600

    after calss project

---
+++ after class code.py
@@ -0,0 +1,13 @@
+# Storing birthdays in 5 different variables
+
+birthday1 = "12-01-2005"
+birthday2 = "23-03-2004"
+birthday3 = "15-06-2006"
+birthday4 = "08-11-2003"
+birthday5 = "29-09-2007"
+
+print(birthday1)
+print(birthday2)
+print(birthday3)
+print(birthday4)
+print(birthday5)
