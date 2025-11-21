a75a41464f4c8ee12532248e6c9e01a5034190b9
paramnesia-7 <rajmoiuksahagmail.com>
2025-11-21 19:58:56 +0600

    python code

---
+++ no-2.py
@@ -0,0 +1,8 @@
+x=10
+y="Reyna"
+print(x)
+print(y)
+codingal = "codingal"
+print(codingal)
+name = input("enter your name")
+print("\nHello", name, "\nwelcome to codingal")
--- no.1.py
+++ no.1.py
@@ -1,5 +1,1 @@
-print("Welcome to the worldof programming!")
-print("4")
-print("Hello Python \n")
-print("hello",5)
-print("Welcome to", end="*")
+
